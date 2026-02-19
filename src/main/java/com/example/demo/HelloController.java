77e65aac2a4f5a3efe90d8b575712a8e972ffe72
Kmpochti <kmpochti@csd.auth.gr>
2026-02-19 20:58:13 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class HelloController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+}
